2541b39bda11d3450159dd9472fc1af7fd36693c
silvestre <22690406@tecvalles.mx>
2026-05-02 22:25:55 -0600

    Añadido vercel.json para fijar error 404 al recargar

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // Esto te permite usar '@' para referirte a la carpeta 'src' 
+      // (ej. import api from '@/api')
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    // Asegura que los archivos se generen en la carpeta que Vercel espera
+    outDir: 'dist',
+  }
 })
